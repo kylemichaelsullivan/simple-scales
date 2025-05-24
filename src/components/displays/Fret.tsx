e25ff508bb5ef513d9971ce838c5170e0eedf568
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-05-24 12:39:06 -0400

    ADD: Note Names to <Fret /> and <Nut />

--- src/components/displays/Fret.tsx
+++ src/components/displays/Fret.tsx
@@ -1,19 +1,25 @@
 import { useIndex } from '../../context';
 
+import AllowedNote from '../AllowedNote';
+
 type FretProps = {
-	note: number;
+  note: number;
 };
 
 function Fret({ note }: FretProps) {
-	const { notes, getNote, playNote } = useIndex();
+  const { notes, getNote, playNote, tonic } = useIndex();
 
-	return (
-		<div
-			className={`Fret border-r border-black w-full${notes.includes(note) ? ' allowed' : ''}`}
-			title={getNote(note)}
-			onClick={() => playNote(note)}
-		></div>
-	);
+  return (
+    <div
+      className="Fret border-r border-black w-full"
+      title={getNote(note)}
+      onClick={() => playNote(note)}
+    >
+      {notes.includes(note) && (
+        <AllowedNote note={getNote(note)} isTonic={note === tonic} />
+      )}
+    </div>
+  );
 }
 
 export default Fret;
--- src/components/displays/Nut.tsx
+++ src/components/displays/Nut.tsx
@@ -1,19 +1,25 @@
 import { useIndex } from '../../context';
 
+import AllowedNote from '../AllowedNote';
+
 type NutProps = {
-	note: number;
+  note: number;
 };
 
 function Nut({ note }: NutProps) {
-	const { notes, getNote, playNote } = useIndex();
+  const { notes, getNote, playNote, tonic } = useIndex();
 
-	return (
-		<div
-			className={`Nut bg-black${notes.includes(note) ? ' allowed' : ''} w-8`}
-			title={getNote(note)}
-			onClick={() => playNote(note)}
-		></div>
-	);
+  return (
+    <div
+      className="Nut bg-black w-8"
+      title={getNote(note)}
+      onClick={() => playNote(note)}
+    >
+      {notes.includes(note) && (
+        <AllowedNote note={getNote(note)} isTonic={note === tonic} />
+      )}
+    </div>
+  );
 }
 
 export default Nut;
