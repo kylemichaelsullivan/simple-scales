9d7524f1a9eb6a5f512b1709b05d3510fe707f73
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-05-24 12:49:10 -0400

    UPDATE: Use color instead of size to show tonic

--- src/components/AllowedNote.tsx
+++ src/components/AllowedNote.tsx
@@ -7,14 +7,14 @@ type AllowedNoteProps = {
 
 function AllowedNote({ note, isTonic }: AllowedNoteProps) {
   const { showNoteLabels } = useIndex();
-  const size = isTonic ? 'w-6 h-6' : 'w-4 h-4';
+  const bgColor = isTonic ? 'bg-green-800' : 'bg-green-600';
   const fontSize = isTonic ? 'text-xxs' : 'text-xxxs';
   const hasFlat = note.includes('♭');
   const hasSharp = note.includes('♯');
 
   return (
     <span
-      className={`AllowedNote absolute flex items-center justify-center bg-green-700 rounded-full text-white font-bold text-center leading-none ${size} ${fontSize} bottom-1 left-1/2 translate-x-[-50%] ${hasFlat ? 'hasFlat' : ''} ${hasSharp ? 'hasSharp' : ''}`}
+      className={`AllowedNote absolute flex items-center justify-center ${bgColor} rounded-full text-white ${fontSize} font-bold text-center leading-none w-4 h-4 bottom-1 left-1/2 translate-x-[-50%] ${hasFlat ? 'hasFlat' : ''} ${hasSharp ? 'hasSharp' : ''}`}
       title={note}
     >
       {showNoteLabels ? note : ''}
