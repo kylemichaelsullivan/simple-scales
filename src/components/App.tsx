8cc4a91e65e08ba633b411c432dd351ef2f6844c
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-05-30 19:03:16 -0400

    REFACTOR: Move <App /> to /components

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Body from './Body';
+
+function App() {
+	return (
+		<div className='App relative flex min-h-screen flex-col gap-4 bg-sky-300'>
+			<Header />
+			<Body />
+		</div>
+	);
+}
+
+export default App;
